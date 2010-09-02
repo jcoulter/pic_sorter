1f28b4064ad6867d1bed4e11f374528f6615fd42
Jacob <Jacob.Coulter@gmail.com>
2010-09-02 09:03:14 -0500

    Jacob: wrapping up my needed functionality - I think

--- pic_loader.rb
+++ pic_loader.rb
@@ -3,38 +3,32 @@
 require File.join(File.dirname(__FILE__), 'pic_sorter')
 require File.join(File.dirname(__FILE__), 'pic')
 
-require 'rubygems'
-require 'rubygems'
 require 'exifr'
 require 'set'
 
 class PicLoader 
 
   def load()
-    puts "HI"
     jpegs =  Dir["./staging/*"]
-    
-    puts "jpegs = #{jpegs}"
-    
-    puts jpegs.class
-    
-    pics = Set.new
-    
-    jpegs.each {|it|  
-    puts EXIFR::JPEG.new(it).width  
-    
-    pic  = Pic.new()
-    pic.file = File.new(it)
-
-    pic.month = EXIFR::JPEG.new(it).date_time.month
-    pic.year = EXIFR::JPEG.new(it).date_time.year
-    pic.file.close
-    pics << pic
-    }
-    
+    pics = create_pics(jpegs)
     
     PicSorter.new.sort(pics)
 
+  end
+  
+  def create_pics(jpegs)
+    pics = Set.new
+    
+    jpegs.each {|it|  
+      pic  = Pic.new()
+      pic.file = File.new(it)
+
+      pic.month = EXIFR::JPEG.new(it).date_time.month
+      pic.year = EXIFR::JPEG.new(it).date_time.year
+      pic.file.close
+      pics << pic
+    }
+    pics
   end
 
 end
--- pic_sorter.rb
+++ pic_sorter.rb
@@ -5,7 +5,6 @@
 
 require File.join(File.dirname(__FILE__), 'pic')
 
-require 'rubygems'
 require 'fileutils'
 require 'set'
 
@@ -14,58 +13,27 @@ class PicSorter
 
 
   def sort(pics)
-    
-
     pics.each {|it| 
-    puts "Sorting #{it} should go to dir #{Dir.pwd}/#{it.dir_string}"
-    
-    FileUtils.mkdir_p "#{it.dir_string}"
-  
-   #`chmod -R 777 2010`
-   
-   #File.move(it.file, "#{Dir.pwd}/#{it.dir_string}")
-
-   puts File.basename(it.file.path)
-   
-   base_name = File.basename(it.file.path)
-   
-   new_file =  it.dir_string + base_name
-   
-   while File.exist?(new_file) do 
-     puts "FILE EXISTS!!!!!!!!!! #{new_file}"
-     
-     base_name = "z" + base_name
-     new_file =  it.dir_string + base_name
-   end
-   
-   FileUtils.mv(it.file.path, new_file)
-   
-   puts it.file.path
-
+      FileUtils.mkdir_p "#{it.dir_string}"
+      FileUtils.mv(it.file.path, generate_file_name(it))
+      puts "Sorted #{it.file.path}"
     }
     
   end
+  
+  def generate_file_name(pic)   
+   base_name = File.basename(pic.file.path)
+   new_file =  pic.dir_string + base_name
+   
+   while File.exist?(new_file) do 
+     puts "Duplicate Name: #{new_file}"
+     
+     base_name = "z" + base_name
+     new_file =  pic.dir_string + base_name
+   end
+   new_file
+  end 
 end
 
 
 
-
-#require 'rubygems'
-#require 'exifr'
-
-#input_file = ARGV[0]
-
-# get the date/time info from the JPEG file
-#d1 = EXIFR::JPEG.new(input_file).date_time
-
-# convert to the format we need
-#d = 'Wed Apr 01 13:51:16 -0800 2009'
-#a,b,d,hms,poop,y = d1.to_s.split
-#d2 = "#{a} #{b} #{d} #{hms} #{y}"
-
-# create a real date
-#d3 = DateTime::strptime(d2, '%a %b %d %H:%M:%S %Y')
-
-# get this date in the desired format
-#s = d3.strftime('%Y%m%d.%H%M%S')
-#puts s
